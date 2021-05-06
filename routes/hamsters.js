bddce903f08fcfd0280f484fa053bff1aca07d5d
Emilie Ågren <emilie.agren@iths.se>
2021-05-06 16:34:26 +0200

    Edit to PUT method and code cleanup

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,17 +1,19 @@
-const getDataBase = require('../database.js')
-const db = getDataBase()
-const express = require('express')
-const router = express.Router()
+const getDataBase = require('../database.js');
+const db = getDataBase();
+const express = require('express');
+const router = express.Router();
 
 
 // GET /hamsters
 
+try {
+
 router.get('/', async (req, res) => {
 
-	const hamstersRef = db.collection('hamsters')
-	const snapshot = await hamstersRef.get()
+	const hamstersRef = db.collection('hamsters');
+	const snapshot = await hamstersRef.get();
 
-	if( snapshot.empty ) {
+	if(snapshot.empty) {
 		res.send([])
 		return
 	}
@@ -25,13 +27,24 @@ router.get('/', async (req, res) => {
 	res.send(hamsterList)
 })
 
+} catch(error) {
+	res.status(500).send('Oops! Something went wrong... ' + error.message);
+}
+
 
 // GET /hamsters/random
 
+try {
+
 router.get('/random', async (req, res) => {
 
-	const hamstersRef = db.collection('hamsters')
-	const snapshot = await hamstersRef.get()
+	const hamstersRef = db.collection('hamsters');
+	const snapshot = await hamstersRef.get();
+
+	if(snapshot.empty) {
+		res.send(404).send('Oh no! No hamsters to be found.');
+		return
+	}
 
 	hamsterList = []
 	snapshot.forEach(doc => {
@@ -40,82 +53,135 @@ router.get('/random', async (req, res) => {
 		hamsterList.push(data)
 	})
 	
-	res.send(hamsterList[Math.floor(Math.random()*hamsterList.length)])
+	res.send(hamsterList[Math.floor(Math.random()*hamsterList.length)]);
 
 } )
+
+} catch(error) {
+	res.status(500).send('Oops! Something went wrong... ' + error.message);
+}
 
 
 // GET /hamsters/:id
 
+try {
+
 router.get('/:id', async (req, res) => {
 
-	const id = req.params.id
-	const hamsterRef = await db.collection('hamsters').doc(id).get()
+	const id = req.params.id;
+	const hamsterRef = await db.collection('hamsters').doc(id).get();
 
-	if( !hamsterRef.exists ) {
-		res.status(404).send('Hamster does not exist')
+	if(!hamsterRef.exists) {
+		res.status(404).send(`Oh no! Hamster with id:${id} does not exist.`);
 		return
 	}
 
-	const data = hamsterRef.data()
-	res.send(data)
+	const data = hamsterRef.data();
+	res.send(data);
 })
+
+} catch(error) {
+	res.status(500).send('Oops! Something went wrong... ' + error.message);
+}
+
 
 // POST /hamsters
 
+try {
+
 router.post('/', async (req, res) => {
 
-	const newHamsterObject = req.body
+	const newHamsterObject = req.body;
 
-	if( !isObject(newHamsterObject) ) {
+	if(!isObject(newHamsterObject)) {
 		res.sendStatus(400)
 		return
 	}
 
-	const docRef = await db.collection('hamsters').add(newHamsterObject)
-	res.send(docRef.id)
+	const docRef = await db.collection('hamsters').add(newHamsterObject);
+	res.send(docRef.id);
 })
 
 function isObject(hamsterObject) {
 
-	if( hamsterObject && ['name', 'age', 'favFood', 'loves', 'imgName', 'wins', 'defeats', 'games'].every(p => hamsterObject.hasOwnProperty(p)))
-		return true
+	if(hamsterObject && ['name', 'age', 'favFood', 'loves', 'imgName', 'wins', 'defeats', 'games'].every(p => hamsterObject.hasOwnProperty(p))) {
 
-	console.log('Not a complete hamster object')	
-	return false	
+		if(!Number.isInteger(hamsterObject.age) || hamsterObject.age < 0) return false;
+		if(!Number.isInteger(hamsterObject.wins) || hamsterObject.wins < 0) return false;
+		if(!Number.isInteger(hamsterObject.defeats) || hamsterObject.defeats < 0) return false;
+		if(!Number.isInteger(hamsterObject.games) || hamsterObject.games < 0) return false;
+
+		return true;
+	}
+		
+	return false;	
+}
+
+} catch(error) {
+	res.status(500).send('Oops! Something went wrong... ' + error.message);
 }
 
 
 // PUT /hamsters/:id
 
+try {
+
 router.put('/:id', async (req, res) => {
 
-	const hamsterObject = req.body
-	const id = req.params.id
+	const hamsterObject = req.body;
+	const id = req.params.id;
+	const hamsterRef = await db.collection('hamsters').doc(id).get();
 
-	if( !hamsterObject || !id ) {
-		req.sendStatus(400)
-		return
+	if(!hamsterObject || !id) {
+		res.sendStatus(400);
+		return;
 	}
 
-	const docRef = db.collection('hamsters').doc(id)
-	await docRef.set(hamsterObject, { merge: true })
-	res.sendStatus(200)
+	if(!hamsterRef.exists) {
+		res.status(404).send('Oh no! Hamster does not exist.');
+		return;
+	}
+
+	const docRef = db.collection('hamsters').doc(id);
+	await docRef.set(hamsterObject, { merge: true });
+
+	if(Object.keys(hamsterObject).length === 0) {
+		res.sendStatus(400);
+		return;
+	}
+	res.sendStatus(200);
 })
 
+} catch(error) {
+	res.status(500).send('Oops! Something went wrong... ' + error.message);
+}
 
 // DELETE /hamsters/:id
+
+try {
+
 router.delete('/:id', async (req, res) => {
 
-	const id = req.params.id
+	const id = req.params.id;
+	const hamsterRef = await db.collection('hamsters').doc(id).get();
 
-	if( !id ) {
-		res.sendStatus(400)
-		return
+	if(!id) {
+		res.sendStatus(400);
+		return;
 	}
 
-	await db.collection('hamsters').doc(id).delete()
-	res.sendStatus(200)
+	if(!hamsterRef.exists) {
+		res.status(404).send('Oh no! Hamster does not exist.');
+		return;
+	}
+
+	await db.collection('hamsters').doc(id).delete();
+	res.sendStatus(200);
 })
 
-module.exports = router
+} catch(error) {
+	res.status(500).send('Oops! Something went wrong... ' + error.message);
+}
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const path = require('path')
-const hamsters = require('./routes/hamsters.js')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const hamsters = require('./routes/hamsters.js');
 
 const PORT = process.env.PORT || 1335
-const staticFolder = path.join(__dirname, 'static')
+const staticFolder = path.join(__dirname, 'static');
 
 // Middleware
 app.use((req, res, next) => {
 	console.log(`${req.method} ${req.url}`, req.params);
-	next()
+	next();
 })
 
-app.use( express.json() )
-app.use( cors() )
-app.use( express.static(staticFolder) )
+app.use( express.json() );
+app.use( cors() );
+app.use( express.static(staticFolder) );
 
 
 // REST API for /hamsters
-app.use('/hamsters', hamsters)
+app.use('/hamsters', hamsters);
 
 // Start server
 app.listen(PORT, () => {
